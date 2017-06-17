f3285c10ebe44bc7064a63632892dc0a1762e057
cocodrips <cocodrips@gmail.com>
2017-06-17 16:54:37 +0900

    [WIP]

--- base.py
+++ base.py
@@ -1,11 +1,11 @@
+from IPython.core.display import display
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import patsy
 import seaborn as sns
 
-from sklearn.cross_validation import train_test_split
-import seaborn as sns
+from sklearn.model_selection import train_test_split
 
 exp_values = [
     'Pclass',
@@ -17,35 +17,6 @@ exp_values = [
 ]
 predict_value = 'Survived'
 child_threthold = 16
-
-survived_ticket_set = {
-    '110152',
-    '113760',
-    '13502',
-    '1601',
-    '24160',
-    '2666',
-    '29106',
-    '347077',
-    '347742',
-    'PC 17572',
-    'PC 17755',
-    'PC 17757'
-}
-
-dead_ticket_set = {
-    '3101295',
-    '345773',
-    '347082',
-    '347088',
-    '349909',
-    '382652',
-    'CA 2144',
-    'LINE',
-    'S.O.C. 14879',
-    'W./C. 6608'
-}
-
 
 def output_result(model, category_data, base_data):
     result = model.predict(category_data)
@@ -60,20 +31,16 @@ def category_to_table(titanic, is_training_data=True):
     if is_training_data:
         fomula = fomula + '+' + predict_value
 
-    print('fomula:', fomula)
-
     c_df = patsy.dmatrix(fomula, data=titanic, return_type='dataframe')
     return c_df
 
 
 def load_titanic_data(is_training_data=True):
     """
-    データを読みこみ、
-    必要なパラメータに絞る
+    データを読みこみ必要なパラメータに絞る
+    
+    :type is_training_data:bool True: train.csv False: test.csv 
     """
-
-    s_rate_ticket_dict = dict()
-    s_count_ticket_dict = dict()
 
     def is_child(age):
         if pd.isnull(age):
@@ -81,16 +48,6 @@ def load_titanic_data(is_training_data=True):
         if age < child_threthold:
             return True
         return False
-
-    def ticket_count_info(x):
-        if x in s_count_ticket_dict:
-            return s_count_ticket_dict[x]
-        return 0
-
-    def ticket_ratio_info(x):
-        if x in s_rate_ticket_dict:
-            return s_rate_ticket_dict[x]
-        return None
 
     titanic = None
     if is_training_data:
@@ -104,19 +61,7 @@ def load_titanic_data(is_training_data=True):
             subset=subset_
         )
 
-        for k, v in titanic.groupby('Ticket')[['Survived']].count().items():
-            print(k)
-            for kk, vv in v.items():
-                s_count_ticket_dict[kk] = vv
-
-        for k, v in titanic.groupby('Ticket')[['Survived']].mean().items():
-            print(k)
-            for kk, vv in v.items():
-                s_rate_ticket_dict[kk] = vv
-
         titanic = titanic.assign(
-            TicketCount=titanic['Ticket'].map(ticket_count_info),
-            TicketSRate=titanic['Ticket'].map(ticket_ratio_info),
             Type="Train",
             Training=1,
             Test=0
@@ -141,27 +86,35 @@ def hit_rate(predict, test):
     return "{0:.3f}%".format(100 * hit / N * 1.0)
 
 
-def main():
-    titanic = load_titanic_data()
-    titanic_category = category_to_table(titanic)
+titanic: pd.DataFrame = load_titanic_data() 
+titanic_female: pd.DataFrame = titanic.query('Sex=="female"')
+titanic_male: pd.DataFrame = titanic.query('Sex=="male"')
 
-    x_columns = list(titanic_category.columns)
-    x_columns.remove(predict_value)
-
-    X_train, X_test, Y_train, Y_test = train_test_split(titanic[x_columns],
-                                                        titanic[predict_value],
-                                                        test_size=0.4,
-                                                        random_state=0)
-
-
-titanic = load_titanic_data()
-titanic.head(2)
-
-## カテゴリカルなデータに変換
 titanic_category = category_to_table(titanic)
-titanic_category.head(2)
+# titanic_category_female = titanic_category.query('"C(Sex)[T.male]"==0')
+# titanic_category_male = titanic_category.query('"C(Sex)[T.male]"==1')
 
-test_data = load_titanic_data(False)
-test_data_category = category_to_table(test_data, False)
+test_titanic = load_titanic_data(False)
+test_titanic_category = category_to_table(test_titanic, False)
 
-full_data = pd.concat([titanic, test_data])
+full_data = pd.concat([titanic, test_titanic])
+
+x_columns = list(titanic_category.columns)
+x_columns.remove(predict_value)
+
+X_train, X_test, Y_train, Y_test = train_test_split(titanic_category[x_columns],
+                                                    titanic_category[predict_value],
+                                                    test_size=0.4,
+                                                    random_state=0)
+
+
+
+def show_basic_data():
+    display('titanic_male')
+    display(titanic_male.head(1))
+    
+    display('titanic')
+    display(titanic.head(1))
+
+    display('titanic_category')
+    display(titanic_category.head(1))
